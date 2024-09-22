7ed8d46dbcfee40d858a9b8ea1e0d789355aa2ed
Ben Christel <ben.christel@gmail.com>
2024-09-22 15:35:29 -0700

    Typecheck TypeScript in the browser

--- devserver/index.tsx
+++ devserver/index.tsx
@@ -1,7 +1,18 @@
 import {render, h} from "preact"
+import { Project } from "../src/project.js"
+import { trimMargin } from "@benchristel/taste"
+
+const project = new Project({
+    "index.ts": trimMargin`
+        const message: number = "Hello, world!"
+        console.log(message)
+    `
+})
+
+const errors = project.getTypeErrors()
 
 render(<Main />, document.getElementById("preact-root")!)
 
 function Main() {
-    return <h1>Hello, world!</h1>
+    return <pre>{JSON.stringify(errors, null, 4)}</pre>
 }
